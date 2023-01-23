86cc0c2ff3f65926149435e07b5610ef1487c245
MobiliDADOS <53263992+mobilidados@users.noreply.github.com>
2023-01-23 16:27:53 -0300

    Update 3-filtrar_infra_ciclo.R

--- pnb/network/3-filtrar_infra_ciclo.R
+++ pnb/network/3-filtrar_infra_ciclo.R
@@ -1,66 +1,100 @@
+# Carregar pacotes
+
 library(stringr)
 library(pbapply)
-library(tableschema.r)
+library(purrr)
+library(dplyr)
+library(sf)
+library(readr)
 
-setwd('E:/R/')
+# Definir diretório de trabalho
 
-#DF e Vitoria nao foram baixadas do Ciclomapa em funcao de erro do app
-munis_df <- data.frame(code_muni = c(2927408, 2611606, 1501402, 1200401, 
-                                     1302603, 1400100, 1600303, 1721000, 
-                                     2211001, 2507507, 2704302, 2800308, 
-                                     3205309, 5002704, 5103403, 5208707),
-                       name_muni=c('salvador', 'recife', 'belem', 'rio branco', 
-                                   'manaus', 'boa vista', 'macapa', 'palmas',
-                                   'teresina', 'joao pessoa', 'maceio', 'aracaju', 
-                                   'vitoria', 'campo grande', 'cuiaba', 'goiania'))
+setwd('D:/Projetos/mobilidados/pnb_novo')
+
+# Definir municípios a baixar
+
+munis_df <- data.frame(code_muni = c(2927408, 3550308, 3304557, 2611606, 
+                                     2304400, 5300108, 4106902,
+                                     3106200, 1501402, 1100205, 1200401,
+                                     1302603, 1400100, 1600303, 1721000, 2111300,
+                                     2211001, 2408102, 2507507, 2704302, 2800308,
+                                     3205309, 4205407, 4314902, 5002704,
+                                     5103403, 5208707),
+                       name_muni=c('salvador', 'sao paulo','rio de janeiro', 'recife',
+                                   'fortaleza', 'distrito federal', 'curitiba', 
+                                   'belo horizonte', 'belem', 'porto velho', 'rio branco', 
+                                   'manaus', 'boa vista', 'macapa', 'palmas', 'sao luis',
+                                   'teresina', 'natal', 'joao pessoa', 'maceio', 'aracaju', 
+                                   'vitoria', 'florianopolis', 'porto alegre', 'campo grande', 
+                                   'cuiaba', 'goiania'),
+                       abrev_state=c('BA', 'SP', 'RJ', 'PE', 'CE', 'DF', 'PR', 'MG', 'PA', 'RO',
+                                     'AC', 'AM', 'RR', 'AP', 'TO', 'MA', 'PI', 'RN', 'PB', 'AL',
+                                     'SE', 'ES', 'SC', 'RS', 'MS', 'MT', 'GO'), 
+                       espg = c(31984, 31983, 31983, 31985, 31984, 31983, 31982, 31983, 31982, 
+                                31980, 31979, 31980, 31980, 31982, 31982, 31983, 31983,
+                                31985, 31985, 31985, 31984, 31984, 31982, 31982, 31981,
+                                31981, 31982),
+                       sigla_muni = c("ssa", "spo", "rio", "rec", "for", "dis", "cur", 
+                                      "bho", "bel", "por", "rbr", "man", "boa", "mac", 
+                                      "pal", "sls", "ter", "nat", "joa", "mco", "ara", 
+                                      "vit", "flo", "poa", "cam", "cui", "goi"))
 
 # to upper name_muni
 munis_df$name_muni <- str_to_title(munis_df$name_muni)
 
-i= 2927408
-rm(i)
+
+# Definir caminho para a pasta com os geojsons dos ciclomapas
+
+# cid <- 3550308 
+# ano <- 2022
 
 
-filtrar_infra_ciclo <- function(i){
+# Antes de começar jogar os arquivos de infra para a pasta de geofabrik
+# Criar
+
+filtrar_infra_ciclo <- function(cid, ano){
   
-  message(paste0('Abrindo infra ciclo - ', subset(munis_df, code_muni==i)$name_muni,"..."))
+  message(paste0('Abrindo infra ciclo - ', subset(munis_df, code_muni==cid)$name_muni,"..."))
   
-  infra_ciclo <- st_read(paste0('./dados/infra_transporte/infra_cicloviaria/2019/vias/ciclomapa_ajustado/ciclomapa-', 
-                                subset(munis_df, code_muni==i)$name_muni,
-                                '.shp')) %>% mutate(type = as.character(type))
+  infra_ciclo <- st_read(paste0('./infra_ciclo/',ano,'/infra_ciclo_', 
+                                subset(munis_df, code_muni==cid)$name_muni,
+                                '.geojson')) %>% st_cast("LINESTRING") # Ler o arquivo geojson baixado do ciclomapa e transforma polígono em "linestring"
+  # Alguns dados no ciclomapa estão em forma de polígono. É necessário modificar para rodar.
   
+  message(paste0('Ajustando infra ciclo - ', subset(munis_df, code_muni==cid)$name_muni,"..."))
   
-  message(paste0('Ajustando infra ciclo - ', subset(munis_df, code_muni==i)$name_muni,"..."))
-  
-  infra_ciclo_pnb <- infra_ciclo[(infra_ciclo$type %in% 
-                                    c('Ciclovia','Ciclofaixa')),]#filtra infraestrutura cicloviaria para considerar apenas ciclovias e ciclofaixas
+  infra_ciclo_pnb <- infra_ciclo[(infra_ciclo$tipologia %in% 
+                                    c('Ciclovia','Ciclofaixa')),]#filtra infraestrutura cicloviaria para considerar apenas ciclovias e ciclofaixas.
   
   infra_ciclo_pnb <- st_transform(infra_ciclo_pnb, 4326)#transforma projecao
-  unique(infra_ciclo_pnb$type)
-  mapview(infra_ciclo) + mapview(infra_ciclo_pnb)
+
+  message(paste0('Salvando infra ciclo - ', subset(munis_df, code_muni==cid)$name_muni,"..."))
   
-  message(paste0('Salvando infra ciclo - ', subset(munis_df, code_muni==i)$name_muni,"..."))
+  # Por segurança salvar em dois formatos os arquivo de infraestrutura cicloviária filtrada
   
-  st_write(infra_ciclo_pnb, paste0('./dados/infra_transporte/infra_cicloviaria/2019/vias/ciclomapa_filtrado/ciclomapa-', 
-                                subset(munis_df, code_muni==i)$name_muni,
+  st_write(infra_ciclo_pnb, paste0('./dados/infra_cicloviaria/geofabrik_filtrado/',ano,'/shp/', 
+                                subset(munis_df, code_muni==cid)$name_muni,
                                 '_filtrado.shp'))
-  
-  # 
-  # write_rds(infra_ciclo_pnb, paste0('E:/R/dados/infra_transporte/infra_cicloviaria/2019/vias/filtrado/', 
-  #                          subset(munis_df, code_muni==i)$name_muni,
-  #                          '_infra_ciclo_filtrada.rds'))
-  
+  write_rds(infra_ciclo_pnb, paste0('./dados/infra_cicloviaria/geofabrik_filtrado/',ano,'/rds/',
+                                   subset(munis_df, code_muni==cid)$name_muni,
+                                   '_filtrado.rds'))
 }
 
+# Cria uma lista com os c?digos a serem iterados na função apply
+
 list_code_muni <- munis_df$code_muni
-pblapply(list_code_muni, filtrar_infra_ciclo)
 
+# Aplica a função de filtrar a lista criada. o Safely é criado para evitar que os erros (cidades sem infra) não travem a função
 
-# cities did not work. need to open on QGIS and save again all together:
-# sao paulo, rio de janeiro, fortaleza, distrito federal, curitiba, 
-# belo horizonte, porto velho, sao luis, florianopolis, porto alegre
-name_muni=c('sao paulo', 'rio de janeiro', 'fortaleza', 'distrito federal', 
-            'curitiba', 'belo horizonte',  'porto velho', 'sao luis', 
-            'natal', 'florianopolis', 'porto alegre')
-sort(munis_df$name_muni)
-sort(name_muni)
+pbmapply(safely(filtrar_infra_ciclo), list_code_muni, ano)
+
+# Junta todos os arquivos rds e coloca em um único arquivo a ser exportado
+
+lista_arquivos <- list.files(paste0("./dados/infra_cicloviaria/geofabrik_filtrado/",ano,"/rds"), pattern='*\\.rds', full.names = TRUE)
+
+caps_lines <- do.call("rbind", lapply(lista_arquivos, read_rds))
+
+# Exporta em dois formatos diferentes por segurança
+
+st_write(caps_lines, paste0("./dados/infra_cicloviaria/geofabrik_filtrado/",ano,"/shp","/geofabrik_capitais_filtrado.shp"), append = TRUE)
+write_rds(caps_lines, paste0("./dados/infra_cicloviaria/geofabrik_filtrado/",ano,"/rds","/geofabrik_capitais_filtrado.rds"))
