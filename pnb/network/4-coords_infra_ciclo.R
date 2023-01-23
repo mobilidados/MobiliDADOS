3ac96dc383a776cf2f201f0b0d29a1d4c3dc2d40
MobiliDADOS <53263992+mobilidados@users.noreply.github.com>
2023-01-23 16:29:07 -0300

    Update 4-coords_infra_ciclo.R

--- pnb/network/4-coords_infra_ciclo.R
+++ pnb/network/4-coords_infra_ciclo.R
@@ -1,34 +1,48 @@
+# O Script importa o arquivo de todas as capitais filtradas e quebra as linhas da infra em pontos para georreferenciá-los
+
+
+# Carrega os pacotes necess?rios
+
 library(sf)
 library(data.table)
 library(dplyr)
+library(stringr)
 library(mapview)
+library(purrr)
+library(readr)
 
-# coords infraestrutura cicloviaria
-# open and reproject infra_ciclo
-infra_ciclo <- st_read('./dados/infra_transporte/infra_cicloviaria/2019/pontos/ciclomapa_capitais_filtrado_pontos.shp')%>%
+# Seleciona a pasta de trabalho
+
+setwd('D:/Projetos/mobilidados/pnb_novo')
+
+# Define o ano de refer?ncia
+
+ano <- 2022
+
+# Importa o arquivo filtrado
+
+infra_ciclo <- read_rds(paste0("./dados/infra_cicloviaria/geofabrik_filtrado/",ano,"/rds","/geofabrik_capitais_filtrado.rds")) %>%
   st_transform(., 4326)
 
+# Quebra a infraestrutura em pontos, com diferença de 5 metros entre si
 
-#clean lat lon
-coords_infra_ciclo <- setDT(infra_ciclo)
-coords_infra_ciclo <- coords_infra_ciclo[, id_points := 1:.N]
-coords_infra_ciclo <- coords_infra_ciclo %>% select(id_points, geometry)
+infra_ciclo_ponts <- st_segmentize(infra_ciclo, 5) %>% st_cast(.,"POINT")
 
-# delete commas
-coords_infra_ciclo <- coords_infra_ciclo[, geometry := ifelse(id_points == nrow(coords_infra_ciclo), 
-                                                              geometry, 
-                                                              stringr::str_sub(geometry, start = 2, end = -1))]
-# delete ()
-coords_infra_ciclo <- coords_infra_ciclo[, geometry := gsub("\\(|\\)", "", geometry)]
+# Transforma geometry em lat e long
 
-# extract coords
-coords_infra_ciclo <- tidyr::separate(coords_infra_ciclo, geometry, c("lat", "lon"), sep = ", ") # remove ","
-coords_infra_ciclo <- coords_infra_ciclo %>% 
-  mutate(lat = ifelse(lat == "c-40.3503139437859", "-40.3503139437859", lat))
+infra_ciclo_ponts <- infra_ciclo_ponts %>%
+  mutate(lat = unlist(map(infra_ciclo_ponts$geometry,1)),
+         lon = unlist(map(infra_ciclo_ponts$geometry,2)))
 
-head(coords_infra_ciclo)
+infra_ciclo_ponts$id_points <- 1:nrow(infra_ciclo_ponts)
 
-coords_infra_ciclo_sf <- st_as_sf(coords_infra_ciclo, coords = c("lat", "lon"), crs = 4326)
-st_write(coords_infra_ciclo_sf, './dados/infra_transporte/infra_cicloviaria/2019/pontos/ciclomapa_coords_capitais.shp')
-fwrite(coords_infra_ciclo, file = "E:/R/dados/infra_transporte/infra_cicloviaria/2019/pontos//ciclomapa_coords_capitais.txt",
-       sep = ",")
+infra_ciclo_ponts <- infra_ciclo_ponts %>% as.data.frame() %>% select(id_points, lat, lon) 
+
+# Transformn the dataframe in sf
+coords_infra_ciclo_sf <- st_as_sf(infra_ciclo_ponts, coords = c("lat", "lon"), crs = 4326)
+
+# Salvando o arquivo em dois formatos para futura importação na próxima etapa
+
+st_write(coords_infra_ciclo_sf, paste0('./dados/infra_cicloviaria/geofabrik_pontos/',ano,'/geofabrik_coords_capitais.shp'), append = TRUE)
+fwrite(infra_ciclo_ponts, file = paste0('./dados/infra_cicloviaria/geofabrik_pontos/',ano,'/geofabrik_coords_capitais.txt'))
+
