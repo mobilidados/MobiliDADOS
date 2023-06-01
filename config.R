54fee269fec5e7440d95183be16742ad7479f9fb
MobiliDADOS <mobilidados@itdp.org>
2023-06-01 16:40:18 -0300

    Script de configuração do git e github

---
+++ config.R
@@ -0,0 +1,21 @@
+# Configurando o git e o github
+
+usethis::use_git_config(user.name = "XXXXXXX",
+                        user.email = "XXXXXXXXXXXXX")
+
+#Criando um token
+
+usethis::create_github_token()
+
+#Editando a variável de ambiente
+
+
+usethis::edit_r_environ()
+
+#Após o último código reiniciar a seção do R
+# CTRL + SHIFT + F10
+
+
+#Testando o acesso
+
+usethis::git_sitrep()
